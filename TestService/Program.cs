e90a43c55d561410fa13628c7f6b91dff6ed96d0
Andrei Dzimchuk <andrei@dzimchuk.net>
2017-11-03 17:22:12 +0300

    migrated TestService to ASP.NET Core 2.0

--- TestService/Program.cs
+++ TestService/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore;
 
 namespace TestService
 {
@@ -12,10 +8,7 @@ namespace TestService
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            var host = WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
 
--- TestService/Startup.cs
+++ TestService/Startup.cs
@@ -1,4 +1,3 @@
-﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,59 +10,34 @@ namespace TestService
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-
-            if (env.IsDevelopment())
-            {
-                builder.AddUserSecrets();
-            }
-
-            Configuration = builder.Build();
+            this.configuration = configuration;
         }
-
-        private IConfigurationRoot Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<AuthenticationOptions>(Configuration.GetSection("Authentication:AzureAd"));
+            services.Configure<AuthenticationOptions>(configuration.GetSection("Authentication:AzureAd"));
 
-            // Add framework services.
+            var provider = services.BuildServiceProvider();
+            var authOptions = provider.GetService<IOptions<AuthenticationOptions>>();
+            
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme) // sets both authenticate and challenge default schemes
+                .AddJwtBearer(options =>
+                {
+                    options.MetadataAddress = $"{authOptions.Value.Authority}/.well-known/openid-configuration?p={authOptions.Value.SignInOrSignUpPolicy}";
+                    options.Audience = authOptions.Value.Audience;
+                });
+
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
             IOptions<AuthenticationOptions> authOptions)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
-            app.UseJwtBearerAuthentication(new JwtBearerOptions
-                                           {
-                                               AutomaticAuthenticate = true,
-                                               AutomaticChallenge = true,
-
-                                               MetadataAddress = $"{authOptions.Value.Authority}/.well-known/openid-configuration?p={authOptions.Value.SignInOrSignUpPolicy}",
-                                               Audience = authOptions.Value.Audience,
-
-                                               Events = new JwtBearerEvents
-                                                        {
-                                                            OnAuthenticationFailed = ctx =>
-                                                                                     {
-                                                                                         ctx.SkipToNextMiddleware();
-                                                                                         return Task.FromResult(0);
-                                                                                     }
-                                                        }
-
-                                           });
-
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
